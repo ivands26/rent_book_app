92529a2443d597d45e05d5c6dd9cbcd80c9a8ed8
unknown <vanilia.cahya23@gmail.com>
2022-07-05 09:48:39 +0700

    Tambah Login

--- entity/users.go
+++ entity/users.go
@@ -24,17 +24,34 @@ func (au *AksesUser) RegisUser(newUser User) User {
 		log.Fatal(err)
 		return User{}
 	}
-
 	return newUser
 }
 
-func (au *AksesUser) GetDataUser() []User {
+func (au *AksesUser) GetUserEmail(EmailUser string) bool {
+	getEmail := au.DB.Where("Email = ?", EmailUser).Find(&User{})
+	if err := getEmail.Error; err != nil {
+		return false
+	}
+	if aff := getEmail.RowsAffected; aff < 1 {
+		return false
+	}
+	return true
+}
+
+func (au *AksesUser) GetUserPass(EmailUser, PassUser string) bool {
+	getPass := au.DB.Where("Email = ? and Password = ?", EmailUser, PassUser).First(&User{})
+	if err := getPass.Error; err != nil {
+		return false
+	}
+	return true
+}
+
+func (as *AksesUser) GetAllData() []User {
 	var daftarUser = []User{}
-	err := au.DB.Find(&daftarUser)
+	err := as.DB.Find(&daftarUser)
 	if err.Error != nil {
 		log.Fatal(err.Statement.SQL.String())
 		return nil
 	}
-
 	return daftarUser
 }
--- main.go
+++ main.go
@@ -14,41 +14,56 @@ func main() {
 
 	var input int
 	for input != 99 {
-		fmt.Println("\tRENT BOOKS APP")
+		fmt.Println("\n\tWelcome in RENT BOOKS APP !!\n")
 		fmt.Println("1. Register")
 		fmt.Println("2. Login")
 		fmt.Println("3. Lihat Koleksi Buku")
-		fmt.Println("99. Keluar")
-		fmt.Println("Masukkan Input Anda :")
+		fmt.Println("10. List User")
+		fmt.Println("99. Keluar\n")
+		fmt.Print("Pilih Menu : ")
 		fmt.Scanln(&input)
 
 		switch input {
 		case 1:
 			var newUser entity.User
-			fmt.Println("Masukan Nama")
+			fmt.Print("\nMasukan Nama: ")
 			fmt.Scanln(&newUser.Nama)
-			fmt.Println("Masukan No HP")
+			fmt.Print("Masukan No HP: ")
 			fmt.Scanln(&newUser.No_hp)
-			fmt.Println("Masukan Email")
+			fmt.Print("Masukan Email: ")
 			fmt.Scanln(&newUser.Email)
-			fmt.Println("Masukan Password")
+			fmt.Print("Masukan Password: ")
 			fmt.Scanln(&newUser.Password)
 			res := aksesUser.RegisUser(newUser)
 			if res.ID == 0 {
 				fmt.Println("Registrasi Gagal")
 				break
 			}
-			fmt.Println("Registrasi Berhasil")
+			fmt.Println("Registrasi Berhasil\n")
 
 		case 2:
-			fmt.Println("Daftar User")
-			for _, value := range aksesUser.GetDataUser() {
-				fmt.Println(value.Nama)
-				fmt.Println(value.No_hp)
-				fmt.Println(value.Email)
-				fmt.Println(" ")
+			var eemail string
+			var pass string
+			fmt.Println("\n--Login--")
+			fmt.Print("Email: ")
+			fmt.Scanln(&eemail)
+			fmt.Print("Password: ")
+			fmt.Scanln(&pass)
+			emailauth := aksesUser.GetUserEmail(eemail)
+			passauth := aksesUser.GetUserPass(eemail, pass)
+			if emailauth == true && passauth == true {
+				fmt.Println("Login Berhasil")
+			} else if emailauth == false || passauth == false {
+				fmt.Println("Email dan Password tidak sesuai, silahkan coba lagi")
 			}
 
+		case 10:
+			fmt.Println("Daftar Seluruh User")
+			for _, val := range aksesUser.GetAllData() {
+				fmt.Println(val)
+			}
+		default:
+			continue
 		}
 	}
 	fmt.Println("TERIMA KASIH TELANG MENGGUNAKAN APLIKASI KAMI")
